cf1a9e4f8fc1c56f0de95164b64bdb1d91d5aadf
mdashikulislam <ashik.nwu@gmail.com>
2023-11-20 23:09:14 +0600

    update font family

--- inc/themes/backend/Stackmin/Views/index.php
+++ inc/themes/backend/Stackmin/Views/index.php
@@ -150,24 +150,24 @@
 
 
                         <!-- Style Switcher -->
-                        <!--                        <li class="nav-item dropdown-style-switcher dropdown me-2 me-xl-0">-->
-                        <!--                            <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);"-->
-                        <!--                               data-bs-toggle="dropdown">-->
-                        <!--                                <i class='bx bx-sm bx-moon'></i>-->
-                        <!--                            </a>-->
-                        <!--                            <ul class="dropdown-menu dropdown-menu-end dropdown-styles">-->
-                        <!--                                <li>-->
-                        <!--                                    <a class="dropdown-item" href="javascript:void(0);" data-theme="light">-->
-                        <!--                                        <span class="align-middle"><i class='bx bx-sun me-2'></i>Light</span>-->
-                        <!--                                    </a>-->
-                        <!--                                </li>-->
-                        <!--                                <li>-->
-                        <!--                                    <a class="dropdown-item" href="javascript:void(0);" data-theme="dark">-->
-                        <!--                                        <span class="align-middle"><i class="bx bx-moon me-2"></i>Dark</span>-->
-                        <!--                                    </a>-->
-                        <!--                                </li>-->
-                        <!--                            </ul>-->
-                        <!--                        </li>-->
+                        <li class="nav-item dropdown-style-switcher dropdown me-2 me-xl-0">
+                            <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);"
+                               data-bs-toggle="dropdown">
+                                <i class='bx bx-sm bx-moon'></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end dropdown-styles">
+                                <li>
+                                    <a class="dropdown-item" href="javascript:void(0);" data-theme="light">
+                                        <span class="align-middle"><i class='bx bx-sun me-2'></i>Light</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="javascript:void(0);" data-theme="dark">
+                                        <span class="align-middle"><i class="bx bx-moon me-2"></i>Dark</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
                         <!-- / Style Switcher-->
 
 
