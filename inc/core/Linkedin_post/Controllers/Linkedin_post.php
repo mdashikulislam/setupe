a0389dac58ad3eeb293962fcf3d855554a0269a3
mdashikulislam <ashik.nwu@gmail.com>
2023-12-06 02:44:50 +0600

    update

--- inc/core/Linkedin_post/Controllers/Linkedin_post.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Core\Linkedin_post\Controllers;
-
-class Linkedin_post extends \CodeIgniter\Controller
-{
-    public function __construct(){
-        $this->config = parse_config( include realpath( __DIR__."/../Config.php" ) );
-    }
-}
